12e5e53c733297905618fcf2f99eecaab90a8fb1
alfonsoruiz <1800ruiz@gmail.com>
2019-11-05 20:45:42 -0800

    Setup endpoints for posts. Added logger middlware globablly

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,68 @@
-const express = 'express';
-
+const express = require('express');
 const router = express.Router();
+const postDB = require('./postDb');
 
 router.get('/', (req, res) => {
-
+  postDB
+    .get()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: 'Post information could not be retrieved' });
+    });
 });
 
 router.get('/:id', (req, res) => {
-
+  postDB
+    .getById(req.params.id)
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Could not retrieve post' });
+    });
 });
 
 router.delete('/:id', (req, res) => {
-
+  postDB
+    .remove(req.params.id)
+    .then(post => {
+      res.status(200).json({ post });
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Post could not be removed' });
+    });
 });
 
 router.put('/:id', (req, res) => {
+  const { text } = req.body;
+  if (!text) {
+    res.status(400).json({ message: 'Please provide text for post' });
+  }
 
+  postDB
+    .update(req.params.id, req.body)
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Could not update post' });
+    });
 });
 
 // custom middleware
+/* 
 
-function validatePostId(req, res, next) {
+- validatePost validates the body on a request to create a new post
 
-};
+- if the request body is missing, cancel the request and respond with status 400 and { message: "missing post data" }
+
+- if the request body is missing the required text field, cancel the request and respond with status 400 and { message: "missing required text field" } 
+*/
+
+function validatePostId(req, res, next) {}
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,29 @@
-const express = 'express';
-
+const express = require('express');
 const server = express();
+server.use(express.json());
+const postRouter = require('./posts/postRouter.js');
+const userRouter = require('./users/userRouter.js');
+
+//Custom middleware. Request handler functions are also part of the middlware chain.
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+      'Origin'
+    )}`
+  );
+
+  // Run next middlware function
+  next();
+}
+
+// Sets logger custom middlware globally to run on all request
+server.use(logger);
+
+server.use('/posts', postRouter);
+server.use('/users', userRouter);
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
-
-//custom middleware
-
-function logger(req, res, next) {
-
-};
 
 module.exports = server;
